c5a26929c599a38f8c764dc52321b4a511f63c3c
jhohan100-7 <hanzzboot2@gmail.com>
2025-10-21 13:56:24 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const tombol sapa = document.getElemenBydl('sapaButton')
+const tombolSapa = document.getElemenById('sapaButton')
 tombolSapa.addEventListener('click', function() {
   alert('Halo! Terima kasih sudah berkunjung!');
 });
